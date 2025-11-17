ad120c0ec47359496c4115f24c690e80fd3647f7
melashri <mohamed.elashri@cern.ch>
2025-11-17 13:52:35 -0500

    fix: add PYTHONPATH to pytest pre-commit hook

--- analysis/tests/utils/__init__.py
+++ analysis/tests/utils/__init__.py
@@ -5,12 +5,12 @@ Provides common functionality for test setup, data generation,
 and result validation across the test suite.
 """
 
-from .test_helpers import *
 from .mock_data_generator import *
+from .test_helpers import *
 
 __all__ = [
-    'assert_arrays_close',
-    'assert_dicts_equal',
-    'create_mock_root_file',
-    'generate_mock_physics_data',
+    "assert_arrays_close",
+    "assert_dicts_equal",
+    "create_mock_root_file",
+    "generate_mock_physics_data",
 ]
